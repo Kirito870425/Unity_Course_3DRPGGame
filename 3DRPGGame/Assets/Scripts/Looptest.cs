3551363d1eb6172d3fc00e1e1517f6e23fa21835
Kirito870425 <50912952+Kirito870425@users.noreply.github.com>
2020-08-26 20:48:40 +0800

    協同程式介紹

---
+++ 3DRPGGame/Assets/Scripts/Looptest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;   //使用協程必須引用此API
+
+public class Looptest : MonoBehaviour
+{
+    public string Corotine;
+    
+    private int MethodB()
+    {
+        return 5;
+    }
+
+    /*協程方法
+    1.傳回類型:IEnumerator
+    2.null 一個影格的時間
+
+    */
+    private IEnumerator Test()
+    {
+        print("我是協程第一行");
+        yield return new WaitForSeconds(3); //協程方法1
+        print("我是兩秒後的城市");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Test());//協程專用呼叫StartCoroutine
+        StartCoroutine(Big());
+    }
+
+    public Transform cube;
+
+    private IEnumerator Big()////每0.2秒三圍向量全部+1
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            cube.localScale += Vector3.one;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
---
+++ 3DRPGGame/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //載入畫面 進度 八條 提示文字
+    public GameObject panel;
+    public Text textLoading;
+    public Text textTip;
+    public Image imageLoading;
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(Loading());
+    }
+    private IEnumerator Loading()
+    {
+        panel.SetActive(true);
+        AsyncOperation ao = SceneManager.LoadSceneAsync("遊戲場景");    //非同步載入資訊 = 非同步載入
+        ao.allowSceneActivation = false;                                //不允許自動載入
+
+
+        while (!ao.isDone)
+        {
+            //progress 載入場景的進度值為0~1 如果設定allow為false會卡在0.9 SO /0.9 = 1
+            textLoading.text = "載入進度:" + (ao.progress / 0.9f * 100).ToString("F2") + "%";
+            imageLoading.fillAmount = ao.progress / 0.9f;
+            yield return null;//一影格執行
+        }
+    }
+}
