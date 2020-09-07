a6bf6d81f66f21777a820e4a4a0c47ed9c0d57e5
Kirito870425 <50912952+Kirito870425@users.noreply.github.com>
2020-09-07 21:30:47 +0800

    角色及攝影機控制

--- 3DRPGGame/Assets/Scripts/CameraControl.cs
+++ 3DRPGGame/Assets/Scripts/CameraControl.cs
@@ -2,13 +2,46 @@
 
 public class CameraControl : MonoBehaviour
 {
+    #region 欄位
+
+    [Header("目標物件")]
     public Transform target;
+    [Header("追蹤速度"), Range(0, 500)]
     public float speed;
+    [Header("旋轉速度"), Range(0, 500)]
     public float turn;
-    public Vector2 limit;
+    [Header("上下角度限制")]
+    public Vector2 limit = new Vector2(45, -30);
+
+    private Quaternion rot;
+
+    #endregion
+
+    #region 方法
 
     public void Track()
     {
+        Vector3 posA = transform.position;
+        Vector3 posB = target.position;
 
+        transform.position = Vector3.Lerp(posA, posB, Time.deltaTime * speed);
+
+        rot.x += Input.GetAxis("Mouse Y") * turn * Time.deltaTime;  //上下角度
+        rot.y -= Input.GetAxis("Mouse X") * turn * Time.deltaTime;  //左右
+
+        rot.x = Mathf.Clamp(rot.x, limit.y, limit.x);               //夾住X在限制內
+        //四元轉歐拉角度(0-360)
+        transform.rotation = Quaternion.Euler(rot.x, rot.y + 180, rot.z);
     }
+
+    #endregion
+    
+    #region 事件
+
+    private void LateUpdate()
+    {
+        Track();
+    }
+
+    #endregion
 }
--- 3DRPGGame/Assets/Scripts/Player.cs
+++ 3DRPGGame/Assets/Scripts/Player.cs
@@ -2,13 +2,23 @@
 
 public class Player : MonoBehaviour
 {
-    #region 屬性
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
+    public float m_speed = 5;
+    [Header("旋轉速度"), Range(0, 1000)]
+    public float m_turn = 5;
+    [Header("攻擊力"), Range(0, 1000)]
+    public float m_attack = 20;
+    [Header("血量"), Range(0, 1000)]
+    public float m_hp = 250;
+    [Header("魔力"), Range(0, 1000)]
+    public float m_mp = 250;
 
-    public float m_speed;
-    public float m_turn;
-    public float m_hp;
-    public float m_mp;
-    public int m_lv;
+    public float m_exp = 0;
+    public int m_lv = 1;
+
+    private Animator ani;
+    private Rigidbody rigi;
 
     #endregion
 
@@ -16,7 +26,17 @@ public class Player : MonoBehaviour
 
     public void Move()
     {
-        
+        //自動判斷A左D右
+        float h = Input.GetAxis("Horizontal");
+        //上下
+        float v = Input.GetAxis("Vertical");
+
+        //Vector3 pos = new Vector3(h, 0, v);                       //世界座標
+        //Vector3 pos = transform.forward * v + transform.right * h;  //區域作標
+        Vector3 pos = cam.forward * v + cam.right * h;  //攝影機的區域作標
+        rigi.MovePosition(transform.position + pos * m_speed * Time.fixedDeltaTime);
+        //ABS絕對值
+        ani.SetFloat("PlayerMove", Mathf.Abs(v) + Mathf.Abs(h));
     }
     public void Attack()
     {
@@ -36,10 +56,23 @@ public class Player : MonoBehaviour
     }
 
     #endregion
+    /// <summary>攝影機根物件</summary>
+    private Transform cam;
 
     #region 事件
 
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rigi = GetComponent<Rigidbody>();
 
+        cam = GameObject.Find("攝影機根物件").transform;  //建議不要在update使用 占用效能
+    }
+
+    private void FixedUpdate()//固定50FPS更新,推薦物理運動
+    {
+        Move();
+    }
 
     #endregion
 
